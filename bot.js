db1a21fc7f23429917be7c6e678d28f8bc788d95
“arunsri7” <arun.i1997@gmail.com>
2020-11-11 19:01:10 +0530

    getting prefix from config and implemented greeting new members in the channel

--- bot.js
+++ bot.js
@@ -1,15 +1,31 @@
-const Discord = require('discord.js')
+// const Discord = require('discord.js')
 require('dotenv').config();
-const client = new Discord.Client()
-key = process.env.API_KEY
-client.login(key)
-client.on('ready',()=> {
-    console.log('Ready bro')
-})
+'use strict';
+
+// Import the discord.js module
+const Discord = require('discord.js');
+// Create an instance of a Discord client
+const client = new Discord.Client();
+const { prefix } = require('./config.json');
+client.login( process.env.API_KEY);
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
 
 client.on('message',(msg)=>{
     var today = new Date();
-    if(msg.content.includes('time')){
+    if(msg.content.startsWith(prefix) && msg.content.toLowerCase().includes('time')){
     msg.reply(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds())
     }
 })
